441698aad34089ddd04049903d07f3c80562fbed
Michiel Staessen <staessenmichiel@gmail.com>
2013-06-16 17:29:33 +0200

    Adds Camera support

--- angular-phonegap.js
+++ angular-phonegap.js
@@ -52,6 +52,31 @@ angular.module('PhoneGap').factory('Accelerometer', [
   }
 ]);
 'use strict';
+var Camera = Camera || {
+    PictureSourceType: {
+      PHOTOLIBRARY: 0,
+      CAMERA: 1,
+      SAVEDPHOTOALBUM: 2
+    },
+    DestinationType: {
+      DATA_URL: 0,
+      FILE_URI: 1,
+      NATIVE_URI: 2
+    },
+    EncodingType: {
+      JPEG: 0,
+      PNG: 1
+    },
+    MediaType: {
+      PICTURE: 0,
+      VIDEO: 1,
+      ALLMEDIA: 2
+    },
+    Direction: {
+      BACK: 0,
+      FRONT: 1
+    }
+  };
 angular.module('PhoneGap').factory('Camera', [
   '$q',
   '$window',
@@ -67,7 +92,12 @@ angular.module('PhoneGap').factory('Camera', [
         PhoneGap.ready().then(function () {
           $window.navigator.camera.cleanup(onSuccess, onError);
         });
-      }
+      },
+      PictureSourceType: Camera.PictureSourceType,
+      DestinationType: Camera.DestinationType,
+      EncodingType: Camera.EncodingType,
+      MediaType: Camera.MediaType,
+      Direction: Camera.Direction
     };
   }
 ]);
